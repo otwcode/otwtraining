3bd228d90269b45a8f8ea863f4e38a7243b55d59
Zooey.Glass04 <Zooey.Glass04@593adb90-213b-11de-9edc-3f40a397ab60>
2009-04-04 17:25:04 +0000

    git-svn-id: http://otwtraining.googlecode.com/svn/trunk@2 593adb90-213b-11de-9edc-3f40a397ab60

---
+++ app/controllers/application.rb
@@ -0,0 +1,16 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '6dcb5f91ab3df952ff7a845b38aa4a47'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
