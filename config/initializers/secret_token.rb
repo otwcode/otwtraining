2d1485dcb40cd510d8fd37b0c17e7221261d7153
jenny.scott.thompson <jenny.scott.thompson@593adb90-213b-11de-9edc-3f40a397ab60>
2010-12-11 08:45:01 +0000

    Attempting to recreate new project for Rails 3
    git-svn-id: http://otwtraining.googlecode.com/svn/trunk@107 593adb90-213b-11de-9edc-3f40a397ab60

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otwtraining::Application.config.secret_token = '7f8103790117d44989aa7792283d68ce66ab328c37538ebb3a97202fd8d79ff6f0a9918d162c99a960b0e4509789abf0e66f1c4dad4e81460035d288e8630e8c'
