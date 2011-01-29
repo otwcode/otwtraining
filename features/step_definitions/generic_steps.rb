df78dd9b004a0ff59eecdb6c7ddd728c95a70bad
Cesy <cesy.avon@gmail.com>
2011-01-29 18:58:40 +0000

    Revert "Revert "Initial attempt at cucumber, doesn't work yet""
    This reverts commit c3d0a15bdb2a822ef2d2bf46c6a22857c6dd0d86.

---
+++ features/step_definitions/generic_steps.rb
@@ -0,0 +1,132 @@
+Then /^debug$/ do
+  debugger
+end
+
+Then /^show me the response$/ do
+  puts page.body
+end
+
+Given /^I wait (\d+) seconds?$/ do |number|
+  Kernel::sleep number.to_i
+end
+
+When 'the system processes jobs' do
+  Delayed::Worker.new.work_off
+end
+
+When 'I reload the page' do
+  visit current_url
+end
+
+Then /^I should see Posted now$/ do
+	now = Time.zone.now.to_s
+  Given "I should see \"Posted #{now}\""
+end
+
+When /^I fill in "([^\"]*)" with$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^I fill in "([^\"]*)" with `([^\`]*)`$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^I fill in "([^\"]*)" with '([^\']*)'$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+Then /^I should find "([^"]*)"(?: within "([^"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    page.find(text)
+  end
+end
+
+Then /^I should find '([^']*)'(?: within "([^"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    page.find(text)
+  end
+end
+
+Then /^I should not find "([^"]*)"(?: within "([^"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    begin
+      wait_until do
+        page.find(text)
+      end
+    rescue Capybara::TimeoutError
+    end
+  end
+end
+
+Then /^I should see the "(alt|title)" text "([^\"]*)"(?: within "([^"]*)")?$/ do |texttype, text, selector|
+  with_scope(selector) do
+    (texttype == "alt") ? (page.should have_xpath("//img[@alt='#{text}']")) : (page.should have_xpath("//img[@title='#{text}']"))
+  end
+end
+
+Then /^I should not see the "(alt|title)" text "([^\"]*)"(?: within "([^"]*)")?$/ do |texttype, text, selector|
+  with_scope(selector) do
+    (texttype == "alt") ? (page.should have_no_xpath("//img[@alt='#{text}']")) : (page.should have_no_xpath("//img[@title='#{text}']"))
+  end
+end
+
+Then /^"([^"]*)" should be selected within "([^"]*)"$/ do |value, field|
+  find_field(field).node.xpath(".//option[@selected = 'selected']").inner_html.should =~ /#{value}/
+end
+
+Then /^I should see "([^"]*)" in the "([^"]*)" input/ do |content, labeltext|
+  find_field("#{labeltext}").value.should == content
+end
+
+When /^"([^\"]*)" is fixed$/ do |what|
+  puts "\nDEFERRED (#{what})"
+end
+
+Then /^the "([^"]*)" checkbox(?: within "([^"]*)")? should be disabled$/ do |label, selector|
+  with_scope(selector) do
+    field_disabled = find_field(label)['disabled']
+    if field_disabled.respond_to? :should
+      field_disabled.should be_true
+    else
+      assert field_disabled
+    end
+  end
+end
+
+Then /^the "([^"]*)" checkbox(?: within "([^"]*)")? should not be disabled$/ do |label, selector|
+  with_scope(selector) do
+    field_disabled = find_field(label)['disabled']
+    if field_disabled.respond_to? :should
+      field_disabled.should be_false
+    else
+      assert !field_disabled
+    end
+  end
+end
+
+Then /^I should find "([^"]*)" selected within "([^"]*)"$/ do |text, selector|
+    if page.respond_to? :should
+      page.should have_content('<option selected="selected" value="' + text + '"')
+    else
+      assert page.has_content?('<option selected="selected" value="' + text + '"')
+    end
+end
+
+# we want greedy matching for this one so we can handle tags that have attributes in them
+Then /^I should see the text with tags "(.*)"$/ do |text|
+  page.body.should =~ /#{text}/m
+end
+
+Then /^I should see the text with tags '(.*)'$/ do |text|
+  page.body.should =~ /#{text}/m
+end
+
+Then /^I should see the page title "(.*)"$/ do |text|
+  within('head title') do
+    page.should have_content(text)
+  end
+end
+
+Then /^the page URL should be "([^"]*)"$/ do |text|
+  current_url.should == text
+end
---
+++ features/view_post.feature
@@ -0,0 +1,6 @@
+Feature: View a post
+	
+	Scenario: View a post
+  Given I am on the home page
+    And I follow "View post"
+  Then show me the page
