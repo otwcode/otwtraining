329b63964ca67a54c1f682008a9799fd5fa6b2d1
Cesy <cesy.avon@gmail.com>
2011-01-29 19:00:18 +0000

    Revert "Adding comments, first attempt"
    This reverts commit c25a1246f6b2996aa01082f31c84aa38b8de4f00.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Otwtraining::Application.routes.draw do
-
-  resources :posts do
-    resources :comments
-    end
+  resources :posts
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
