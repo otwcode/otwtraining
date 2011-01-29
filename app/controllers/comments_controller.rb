c25a1246f6b2996aa01082f31c84aa38b8de4f00
Cesy <cesy.avon@gmail.com>
2011-01-29 16:59:22 +0000

    Adding comments, first attempt

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,90 @@
+class CommentsController < ApplicationController
+  before_filter :load_post
+
+  # GET /comments
+  # GET /comments.xml
+  def index
+    @comments = Comment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @comments }
+    end
+  end
+
+  # GET /comments/1
+  # GET /comments/1.xml
+  def show
+    @comment = Comment.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @comment }
+    end
+  end
+
+  # GET /comments/new
+  # GET /comments/new.xml
+  def new
+    @comment = Comment.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @comment }
+    end
+  end
+
+  # GET /comments/1/edit
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  # POST /comments
+  # POST /comments.xml
+  def create
+    @comment = Comment.new([params[:post], params[:comment]])
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to([@post, @comment], :notice => 'Comment was successfully created.') }
+        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /comments/1
+  # PUT /comments/1.xml
+  def update
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to([@post, @comment], :notice => 'Comment was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.xml
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(post_comments_url(@post)) }
+      format.xml  { head :ok }
+    end
+  end
+  
+  def load_post
+    @post = Post.find(params[:post_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Otwtraining::Application.routes.draw do
-  resources :posts
+
+  resources :posts do
+    resources :comments
+    end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
