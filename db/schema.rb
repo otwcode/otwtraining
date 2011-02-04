34b1436255e500385bedfe33f843b7c4b76c03e1
bingeling <bingeling@gmail.com>
2011-02-04 23:09:39 +0000

    setting up otwtraining

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101211084920) do
-
-  create_table "comments", :force => true do |t|
-    t.integer  "post_id"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20101211084400) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
